ce5af734738c381d38a5f4b2ce13ed9da7d22d7c
ak0513 <minimar0812@gmail.com>
2024-08-09 17:32:55 +0900

    html

--- 운동/js/운동.js
+++ 운동/js/운동.js
@@ -39,7 +39,14 @@ function loadData(url, targetElementId, callback) {
 }
 
 function callback() {
-    
+    var toggleBtn = document.querySelectorAll('.toggle');
+    console.log(toggleBtn)
+    toggleBtn.forEach(function(item) {
+        item.addEventListener('click', function() {
+            this.nextElementSibling.classList.toggle('on')
+            console.log(this.nextElementSibling)
+        })
+    })
 }
 
 loadData(Shoulder, 'Shoulder', callback);
